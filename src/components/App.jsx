77046265c8105ffbf324570ff032243fcbefd77a
Lesia Sasiuk <150070945+Cassel444@users.noreply.github.com>
2024-06-24 16:03:05 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { Layout } from "./Layout/Layout";
+import { AppBar } from "./AppBar/AppBar";
+import { TaskForm } from "./TaskForm/TaskForm";
+import { TaskList } from "./TaskList/TaskList";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchTasks } from "../redux/operations";
+import { selectError, selectIsLoading } from "../redux/selectors";
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      {error && <b>Ooops... something went wrong</b>}
+      {isLoading && <b>Loading task...</b>}
+      <TaskList />
+    </Layout>
+  );
+};
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,20 @@
+import { useDispatch } from "react-redux";
+import css from "./Task.module.css";
+import { deleteTask, toggleCompleted } from "../../redux/operations";
+
+export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
+
+  return (
+    <div className={css.wrapper}>
+      <input type="checkbox" checked={task.completed} onChange={handleToggle} />
+      <p className={css.text}>{task.text}</p>
+      <button className={css.btn} onClick={handleDelete} type="button">
+        Delete
+      </button>
+    </div>
+  );
+};
